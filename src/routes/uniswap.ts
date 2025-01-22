f156ecd94e5d42a8321323662636e7b476186b90
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-01-22 21:51:43 +0100

    Try align with Schema

--- src/routes/uniswap.ts
+++ src/routes/uniswap.ts
@@ -25,9 +25,9 @@ router.post("/", async (req: Request, res: Response) => {
     console.log("Responding with", orderData);
     return res.status(200).json(orderData);
   } catch (e: unknown) {
-    const message = JSON.stringify((e as Error).message);
+    const message = (e as Error).message;
     console.error(message);
-    return res.status(400).json({ error: message });
+    return res.status(400).json({ ok: false, message });
   }
 });
 
